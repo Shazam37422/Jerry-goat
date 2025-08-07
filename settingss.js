4e14d9152c6395faa2cc46eedf047a4368445f0f
Shazam37422 <1804disney@gmail.com>
2025-08-07 14:12:51 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~kqtRmaLb#lPcujUByNkIYaJqkczyyOzxGij2OtzCx0VAR58PB6cg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+19412501819", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Eastern Daylight Time" //put your country timeZone....leave blank if u don't know.
 };
